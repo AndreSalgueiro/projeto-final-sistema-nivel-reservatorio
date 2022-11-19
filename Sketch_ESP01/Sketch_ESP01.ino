e3440081205f631050616f9943aee88ed23334dd
andrecoordena <andrecoordena@gmail.com>
2022-11-19 09:31:19 -0300

    alteracao funcao calcular nivel

--- Sketch_ESP01/Sketch_ESP01.ino
+++ Sketch_ESP01/Sketch_ESP01.ino
@@ -1,13 +1,13 @@
-// Inclusao de biblioteca
+//Inclusao de biblioteca
 #include "config.h"
 #include <NewPing.h>
 //declarando de constantes
 #define GPIO_TRIG 0
 #define GPIO_ECHO 2
-#define MAX_DISTANCIA 100
+#define MAX_DISTANCIA 150
 #define DELAY_LEITURA 5000
-#define VOLUME_REAL 35.00
-#define NIVEL_CHEIO 95//% 
+#define DIMENSAO_EFETIVA 65.00//cm de agua para reservatorio cheio
+#define NIVEL_CHEIO 100//% 
 #define NIVEL_VAZIO 40//%
 //Instanciando objetos
 AdafruitIO_Feed *nivel = io.feed("nivelIO");
@@ -28,23 +28,23 @@ void setup() {
   Serial.println("Conectado");
   //se escreve nesse topico vai executar a funcao assim que o Adafruito IO publicar novo dado
   controleAutomatico->onMessage(mensagemRecebidaCtrlAutomatico); 
-  //apos conectar, obtem imediatamente o ultimo valor gravado deste feed e aciona a funcao mensagemRecebida...parar obter o ultomo valor publicado
+  //apos conectar, obtem imediatamente o ultimo valor gravado deste feed e aciona a funcao mensagemRecebida...parar obter o ultimo valor publicado
   controleAutomatico->get();
 }
 void loop() {
   io.run();//mantem-se conectado
-  if ( millis() - ultimaLeitura > DELAY_LEITURA) {   
-    int distanciaAtual = sonar.ping_cm();
+  if ( millis() - ultimaLeitura > DELAY_LEITURA) { 
+    int medidaNivel = calculaNivel();
     //calcula porcentagem disponivel no reservatorio
-    float aux = (100 / VOLUME_REAL);
-    float aux2 = aux * (distanciaAtual - 25);
+    float aux = (100 / DIMENSAO_EFETIVA);
+    float aux2 = aux * (medidaNivel - 35);//o valor 35 e o desconto do sensor ate a parte mais cheia do reservatorio
     int porcentagemNivel = 100 - aux2;
     //ajuste para nao ocorrer valores > 100 e < 0
     if(porcentagemNivel > 100){
       porcentagemNivel = 100;
     }else if(porcentagemNivel < 0){
       porcentagemNivel = 0;
-    }  
+    }
     nivel->save(porcentagemNivel);//publica no topico de nivel
     if( porcentagemNivel >= NIVEL_CHEIO ){//Se o reservatorio esta cheio
       bomba->save(LOW);//publica no topico da bomba    
@@ -54,7 +54,16 @@ void loop() {
     ultimaLeitura = millis();
   }  
 }
-//Implemento de funcao
+//Implemento de funcoes
 void mensagemRecebidaCtrlAutomatico(AdafruitIO_Data *data) {
   statusAutomatico = data->toInt(); //obtem dados do topico controle automatico bomba
+}
+int calculaNivel(){
+  int resultado = 0, contador = 0;   
+  while (resultado == 0 && contador < 30){
+    delay(10);    
+    resultado = sonar.ping_cm(); 
+    contador++;
+  } 
+  return  resultado;
 }
